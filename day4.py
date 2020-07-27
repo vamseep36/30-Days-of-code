839c25170f03a57c617d875937d0c5f13071ee10
Vishnu Vamsee Paningipalli <62656098+sslgstat@users.noreply.github.com>
2020-07-27 12:41:18 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -7,8 +7,6 @@ class Person:
             print ('Age is not valid, setting age to 0.')
         else: 
             initialAge = self.age
-
-
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
         if self.initialAge < 13:
@@ -18,12 +16,10 @@ class Person:
         else:
             print ('You are old.')
 
-
     def yearPasses(self):
-        # Increment the age of the person in here      
+        # Increment the age of the person in here
         self.initialAge += 1
-        
-    
+
 t = int(input())
 for i in range(0, t):
     age = int(input())         
